d2a1795c4c8ebfef13f34414a2b101ada3fb1619
LeeYeJi <lwj1748@naver.com>
2019-06-29 21:57:17 +0900

    Add feature about market

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CounterStore from './stores/counter'; // 방금 만든 스토어 불러와줍니다.
+import MarketStore from './stores/market';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = new CounterStore();
+const market = new MarketStore();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <Provider counter={counter} market={market}>
+    {/* Provider 에 props 로 넣어줍니다. */}
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 serviceWorker.unregister();
